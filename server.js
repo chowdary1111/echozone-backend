119740d8c4b0d82cc0e511cbfa110386803c4959
chetan sai chowdary <chetansairaparla433@gmail.com>
2026-03-11 19:07:53 +0530

    fix MONGODB connection

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(cors());
 
 // Connect to MongoDB
-mongoose.connect("mongodb://127.0.0.1:27017/ECHOZONE")
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB Connected"))
 .catch(err => console.log(err));
 
