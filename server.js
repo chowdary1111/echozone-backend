ee64481a0feffab1e330cb9d37305bde8cba8da6
chetan sai chowdary <chetansairaparla433@gmail.com>
2026-03-11 18:01:38 +0530

    fix render port

--- server.js
+++ server.js
@@ -73,6 +73,8 @@ res.status(500).send("Error fetching posts");
 });
 
 
-app.listen(3000, () => {
-console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+console.log("server running on port " + PORT);
 });
