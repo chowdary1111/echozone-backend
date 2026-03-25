27ed2ca9a7df93fbb3eb24ec848b750d29917651
chetan sai chowdary <chetansairaparla433@gmail.com>
2026-03-25 17:25:22 +0530

    Emergency page UI fixed and modified sound issues

--- public/emergency.js
+++ public/emergency.js
@@ -1,85 +1,76 @@
-// 🚨 Emergency Hold System
+// emergency.js
 
-let holdButton = document.querySelector(".hold-button");
-let timerText = document.getElementById("time");
-let cancelButton = document.getElementById("cancel-btn");
+const holdCircle = document.getElementById('holdCircle');
+const stopAlarmBtn = document.getElementById('stopAlarm');
+const emergencySound = document.getElementById('emergencySound');
 
-let holdTime = 10;
-let interval;
+let holdTimer = null;
+let vibrationInterval = null;
+const HOLD_DURATION = 2500;
 
+function canVibrate() {
+  return 'vibrate' in navigator;
+}
 
+function shortHaptic() {
+  if (canVibrate()) navigator.vibrate(60);
+}
 
-// HOLD START
-holdButton.addEventListener("mousedown", startHold);
-holdButton.addEventListener("touchstart", startHold);
+function startPulsingHaptic() {
+  if (!canVibrate()) return;
+  stopHaptic();
+  vibrationInterval = setInterval(() => navigator.vibrate([90, 130]), 420);
+}
 
-// HOLD RELEASE
-holdButton.addEventListener("mouseup", cancelHold);
-holdButton.addEventListener("mouseleave", cancelHold);
-holdButton.addEventListener("touchend", cancelHold);
+function stopHaptic() {
+  if (canVibrate()) navigator.vibrate(0);
+  if (vibrationInterval) clearInterval(vibrationInterval);
+}
 
-function startHold() {
+function activationHaptic() {
+  if (canVibrate()) navigator.vibrate([180, 90, 280, 120, 180]);
+}
 
-    holdTime = 10;
+function playEmergencySound() {
+  emergencySound.volume = 1;
+  emergencySound.loop = true;
+  emergencySound.play().catch(err => console.error("Sound blocked:", err));
+}
 
-    timerText.innerText =
-    "Activating in " + holdTime + " seconds...";
+function stopEmergencySound() {
+  emergencySound.pause();
+  emergencySound.currentTime = 0;
+}
 
-    interval = setInterval(() => {
+function startHold(e) {
+  e.preventDefault();
+  shortHaptic();
+  startPulsingHaptic();
 
-        holdTime--;
-
-        timerText.innerText =
-        "Activating in " + holdTime + " seconds...";
-
-        if (holdTime <= 0) {
-
-            clearInterval(interval);
-
-            activateEmergency();
-
-        }
-
-    }, 1000);
-
+  holdTimer = setTimeout(() => {
+    stopHaptic();
+    activationHaptic();
+    console.log("🚨 EMERGENCY ACTIVATED!");
+    playEmergencySound();
+    alert("🚨 EMERGENCY ACTIVATED!\nSound is playing.");
+  }, HOLD_DURATION);
 }
 
 function cancelHold() {
-
-    clearInterval(interval);
-
-    timerText.innerText =
-    "Hold the circle to activate emergency";
-
+  stopHaptic();
+  if (holdTimer) clearTimeout(holdTimer);
 }
 
-function activateEmergency() {
+holdCircle.addEventListener('mousedown', startHold);
+holdCircle.addEventListener('touchstart', startHold);
+holdCircle.addEventListener('mouseup', cancelHold);
+holdCircle.addEventListener('mouseleave', cancelHold);
+holdCircle.addEventListener('touchend', cancelHold);
+holdCircle.addEventListener('touchcancel', cancelHold);
 
-    timerText.innerText =
-    "🚨 EMERGENCY ACTIVATED";
-
-   document.getElementById("alarmSound").play();
-
-}
-
-cancelButton.addEventListener("click", () => {
-
-    clearInterval(interval);
-
-    timerText.innerText =
-    "Emergency Cancelled";
-
-  document.getElementById("alarmSound").pause();
+stopAlarmBtn.addEventListener('click', () => {
+  cancelHold();
+  stopEmergencySound();
+  if (canVibrate()) navigator.vibrate([70, 50, 70]);
+  alert("Emergency stopped.");
 });
-function stopAlarm() {
-
-const alarm = document.getElementById("alarmSound");
-
-if (alarm) {
-
-alarm.pause();
-alarm.currentTime = 0;
-
-}
-
-}
